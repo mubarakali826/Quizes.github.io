031542ff0fbb2e23da92068ea36b26e667078752
mubarakali826 <122211700+mubarakali826@users.noreply.github.com>
2024-08-23 01:32:33 +0500

    initial commit

---
+++ questions.js
@@ -0,0 +1,167 @@
+let questions = [
+  {
+      numb: 1,
+      question: "Which HTML tag is used to create a form?",
+      answer: "form",
+      options: [
+        "input",
+        "form",
+          "textarea",
+          "button"
+      ]
+  },
+  {
+      numb: 2,
+      question: "How do you define an input field in an HTML form?",
+      answer: "input",
+      options: [
+        "field",
+        "textbox",
+        "text",
+        "input"
+      ]
+  },
+  {
+      numb: 3,
+      question: "What attribute of the input tag specifies the type of input?",
+      answer: "type",
+      options: [
+          "value",
+          "type",
+          "name",
+          "placeholder"
+      ]
+  },
+  {
+      numb: 4,
+      question: "Which HTML tag is used to create a table?",
+      answer: "table",
+      options: [
+        "tr",
+        "td",
+        "table",
+          "thead"
+      ]
+  },
+  {
+      numb: 5,
+      question: "How do you define a row in an HTML table?",
+      answer: "tr",
+      options: [
+        "td",
+        "th",
+        "table",
+        "tr"
+      ]
+    },
+  {
+      numb: 6,
+      question: "Which tag is used to create a table header cell?",
+      answer: "th",
+      options: [
+        "td",
+        "th",
+        "tr",
+        "table"
+      ]
+  },
+  {
+      numb: 7,
+      question: "What does the ul tag represent in HTML?",
+      answer: "an unordered list",
+      options: [
+          "an ordered list",
+          "a list item",
+          "an unordered list",
+          "a definition list"
+      ]
+  },
+  {
+      numb: 8,
+      question: "How do you create a list item in an HTML list?",
+      answer: "li",
+      options: [
+        "li",
+          "list",
+          "item",
+          "ul"
+      ]
+  },
+  {
+      numb: 9,
+      question: "What is the purpose of the h1 tag in HTML?",
+      answer: "It defines the largest heading.",
+      options: [
+          "It defines a paragraph.",
+          "It defines a section.",
+          "It defines the largest heading.",
+          "It defines a footer."
+      ]
+  },
+  {
+      numb: 10,
+      question: "Which HTML tag is self-closing?",
+      answer: "img",
+      options: [
+          "div",
+          "span",
+          "img",
+          "a"
+      ]
+  },
+  {
+      numb: 11,
+      question: "What is the purpose of the form element in HTML?",
+      answer: "It is used to collect user input.",
+      options: [
+          "It is used to create a table.",
+          "It is used to link external resources.",
+          "It is used to collect user input.",
+          "It is used to define a list."
+      ]
+  },
+  {
+      numb: 12,
+      question: "Which HTML tag is used to define a block of text in a form that can be edited and stetched?",
+      answer: "textarea",
+      options: [
+          "input",
+          "textarea",
+          "button",
+          "select"
+      ]
+  },
+  {
+      numb: 13,
+      question: "How do you specify a cell in a table body?",
+      answer: "td",
+      options: [
+          "cell",
+          "td",
+          "tr",
+          "th"
+      ]
+  },
+  {
+      numb: 14,
+      question: "What HTML tag would you use to create a dropdown list?",
+      answer: "select",
+      options: [
+          "dropdown",
+          "select",
+          "input",
+          "option"
+      ]
+  },
+  {
+      numb: 15,
+      question: "Which tag is used to create the body of a table? ",
+      answer: "tbody",
+      options: [
+          "body",
+          "tbody",
+          "header",
+          "tr"
+      ]
+  }
+];
